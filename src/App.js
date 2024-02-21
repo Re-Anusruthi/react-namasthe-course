a678a8c464bf7bb30d7e5ec81e04c0dfce206a7d
Anusruthi <rejulanusruthi@gmail.com>
2024-02-21 16:23:21 +0530

    App Optimization-Chunking/Lazyloading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Suspense, lazy}  from "react";
 import ReactDOM from "react-dom/client";
 import Header from "../src/Header";
 import Body from "../src/Body";
@@ -7,6 +7,8 @@ import Contact from "./Contact";
 import Error from "./Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import RestaurantMenu from "./RestaurantMenu";
+
+const Grocery = lazy(() => import("./Grocery"));
 
 const Applayout = () => (
     <div className="app-layout">
@@ -31,6 +33,10 @@ const routePath = createBrowserRouter([
             {
                 path: "/contact",
                 element: <Contact/>
+            },
+            {
+                path: "/grocery",
+                element: <Suspense fallback = {<h1>Loading!!</h1>}><Grocery/></Suspense>
             },
             {
                 path: "/restaurants/:resid",
